28c5e3227858549eaeb2cf0185a27becec980d99
Bootloopy <uvvuisaac@gmail.com>
2023-08-21 22:16:10 -0400

    Testing

--- assets/global/js/template.js
+++
@@ -1,9 +0,0 @@
-function footer() {
-  	var parent = document.querySelector("body");
-  	var footer = document.createElement("footer");
-  	var footerText = document.createElement("div");
-  	footerText.classList.add("footerText");
-  	footerText.innerText = "Copyright © 2023 Universe Dev";
-  	footer.appendChild(footerText);
-  	parent.insertBefore(footer, parent.lastChild);
-}
