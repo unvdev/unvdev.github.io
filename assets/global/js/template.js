9b75fe7672c34b39a98b6f3f0e56ee43f7196d23
Bootloopy <uvvuisaac@gmail.com>
2023-08-21 22:03:36 -0400

    Update template.js

--- assets/global/js/template.js
+++ assets/global/js/template.js
@@ -1,11 +1,9 @@
-window.addEventListener("load", (event) => {
-  	function footer() {
-  		var parent = document.querySelector("body");
-  		var footer = document.createElement("footer");
-  		var footerText = document.createElement("div");
-  		footerText.classList.add("footerText");
-  		footerText.innerText = "Copyright © 2023 Universe Dev";
-  		footer.appendChild(footerText);
-  		parent.insertBefore(footer, parent.lastChild);
-  	}
-});
+function footer() {
+  	var parent = document.querySelector("body");
+  	var footer = document.createElement("footer");
+  	var footerText = document.createElement("div");
+  	footerText.classList.add("footerText");
+  	footerText.innerText = "Copyright © 2023 Universe Dev";
+  	footer.appendChild(footerText);
+  	parent.insertBefore(footer, parent.lastChild);
+}
