df693a339eab49d395532190331f1818b4778845
Bootloopy <uvvuisaac@gmail.com>
2023-08-21 21:39:58 -0400

    testing

---
+++ assets/global/js/template.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", (event) => {
+  	function footer() {
+  		var parent = document.querySelector("body");
+  		var footer = document.createElement("footer");
+  		var footerText = document.createElement("div");
+  		footerText.classList.add("footerText");
+  		footer.appendChild(footerText);
+  		parent.prepend(footer, parent);
+  	}
+  	footer();
+});
