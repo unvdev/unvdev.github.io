aa5ba9fe686c73598d4ef95f9c21c0185ed0e67b
unvdev <142684085+unvdev@users.noreply.github.com>
2025-08-28 11:44:47 -0400

    Update galleries.js

--- galleries.js
+++ galleries.js
@@ -1,9 +1,22 @@
-fetch('/assets/images/about/gallery.json')
-  .then(res => res.json())
-  .then(files => {
-    files.forEach(file => {
-      const img = document.createElement('img');
-      img.src = `/assets/images/about/${file}`;
-      document.querySelector('#gallery').appendChild(img);
+const folderId = "YOUR_FOLDER_ID";
+const apiKey = "YOUR_API_KEY";
+
+async function getFolderImages() {
+    const response = await fetch(`https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`);
+    const data = await response.json();
+
+    const gallery = document.getElementById("gallery");
+
+    data.files.forEach(file => {
+        if(file.mimeType.startsWith("image/")) {
+            const img = document.createElement("img");
+            img.src = `https://drive.google.com/uc?export=view&id=${file.id}`;
+            img.alt = file.name;
+            img.style.width = "200px";
+            img.style.margin = "5px";
+            gallery.appendChild(img);
+        }
     });
-  });
+}
+
+getFolderImages();
