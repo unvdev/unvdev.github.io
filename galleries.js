7eba23c6afe1c01d254b2d6697ce9564a2baf23e
unvdev <142684085+unvdev@users.noreply.github.com>
2025-08-28 11:28:40 -0400

    Create galleries.js

---
+++ galleries.js
@@ -0,0 +1,9 @@
+fetch('/assets/images/about/gallery.json')
+  .then(res => res.json())
+  .then(files => {
+    files.forEach(file => {
+      const img = document.createElement('img');
+      img.src = `/assets/images/about/${file}`;
+      document.querySelector('#gallery').appendChild(img);
+    });
+  });
